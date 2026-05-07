f8bb532be9995694007ec6ad528a6c50548061c5
camellia <2877424914@qq.com>
2026-05-07 18:07:48 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -2,16 +2,16 @@ package com.sky.controller.user;
 
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
 import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -28,5 +28,18 @@ public class ShoppingCartController {
         log.info("添加购物车,商品信息为：{}", shoppingCartDTO);
         shoppingCartService.add(shoppingCartDTO);
         return Result.success();
+    }
+    /**
+     * 查看购物车
+     * 获取当前用户的购物车商品列表
+     *
+     * @return Result<List<ShoppingCart>> 返回封装的购物车商品列表结果
+     */
+    @GetMapping("/list")
+    @ApiOperation("查看购物车")
+    public Result<List<ShoppingCart>> list(){
+        log.info("查看购物车");
+        List<ShoppingCart> list = shoppingCartService.showShoppingCart();
+        return Result.success(list);
     }
 }
